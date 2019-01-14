f8dc859a99730a4fa91a1ee9df5490b27915aef4
Mehran Naghizadeh <tarhineh@gmail.com>
2019-01-14 14:33:05 +0000

    If a bericht contains a file, its view is expanded by default

--- app/components/berichtencentrum/bericht-view.js
+++ app/components/berichtencentrum/bericht-view.js
@@ -3,6 +3,13 @@ import Component from '@ember/component';
 export default Component.extend({
   isExpanded: false,
 
+  didReceiveAttrs() {
+    if (this.bericht.inhoud == '888')
+      this.bericht
+        .get('bijlagen')
+        .then(results => this.set("isExpanded", results.length > 0));
+  },
+
   actions: {
     expand() {
       this.set("isExpanded", !this.isExpanded);
