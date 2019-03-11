985eccab187dd851fbcfe9faf1b82e99ade34ce1
Aad Versteden <madnificent@gmail.com>
2019-03-11 23:01:33 +0100

    New tests for berichtencentrum
    Better testing of the available pages based on access rights.  I tried
    to convey what you could see and what you couldn't see, without
    enforcing specific behaviour on the site.  Eg: when the correct access
    rights are not available, we don't really care about what happens, as
    long as you don't see the berichtencentrum page.

--- tests/acceptance/berichtencentrum/berichten-test.js
+++ tests/acceptance/berichtencentrum/berichten-test.js
@@ -1,14 +1,41 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
-import { currentSession, authenticateSession, invalidateSession} from 'ember-simple-auth/test-support';
+import session from '../../helpers/session';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Acceptance | berichtencentrum/berichten', function(hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
 
   test('visiting /berichtencentrum/ without login redirects to /login', async function(assert){
     await visit('/berichtencentrum');
 
     assert.equal(currentURL(), '/login');
   });
+
+  test('visiting /berichtencentrum/ with login shows berichtencentrum', async function(assert){
+    await session( this.server );
+
+    await visit('/berichtencentrum/');
+
+    assert.ok(currentURL().startsWith("/berichtencentrum"));
+  });
+
+  test('visiting /berichtencentrum/ with only berichten access shows berichtencentrum', async function(assert){
+    await session( this.server, { roles: ["LoketLB-berichtenGebruiker"] } );
+
+    await visit('/berichtencentrum/');
+
+    assert.ok(currentURL().startsWith("/berichtencentrum"));
+  });
+
+  test('visiting /berichtencentrum/ with incorrect access rights does not show berichtencentrum', async function(assert){
+    await session( this.server, { roles: ["LoketLB-bbcdrGebruiker"] } );
+
+    await visit('/berichtencentrum/');
+
+    assert.notOk(currentURL().startsWith("/berichtencentrum"));
+  });
+
 });
