9b2c9c870a8492a0562a7a1305d3245e03550c9d
Aad Versteden <madnificent@gmail.com>
2019-03-11 16:56:22 +0100

    Berichtencentrum integration test: redirect to login
    We must redirect to the login page when we're not logged in.

--- tests/acceptance/berichtencentrum/berichten-test.js
+++ tests/acceptance/berichtencentrum/berichten-test.js
@@ -1,15 +1,14 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import { currentSession, authenticateSession, invalidateSession} from 'ember-simple-auth/test-support';
 
 module('Acceptance | berichtencentrum/berichten', function(hooks) {
   setupApplicationTest(hooks);
 
+  test('visiting /berichtencentrum/ without login redirects to /login', async function(assert){
+    await visit('/berichtencentrum');
 
-
-  test('visiting /berichtencentrum/berichten', async function(assert) {
-    await visit('/berichtencentrum/berichten');
-
-    assert.equal(currentURL(), '/berichtencentrum/berichten');
+    assert.equal(currentURL(), '/login');
   });
 });
