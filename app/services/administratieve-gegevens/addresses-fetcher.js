ce38449d5a4b4a81d458d7380ab86e65a81e0e64
Claire Lovisa <lovisa.claire@gmail.com>
2019-05-21 09:45:07 +0200

    Use v4 insead of v3 for loc.geopunt

--- app/services/administratieve-gegevens/addresses-fetcher.js
+++ app/services/administratieve-gegevens/addresses-fetcher.js
@@ -5,7 +5,7 @@ export default Service.extend({
   ajax: service(),
 
   async fetchAddresses(fuzzyAddress) {
-    const addresses = await this.get('ajax').request(`http://loc.geopunt.be/v3/Location?q=${fuzzyAddress}`, {
+    const addresses = await this.get('ajax').request(`http://loc.geopunt.be/v4/Location?q=${fuzzyAddress}`, {
         method: 'GET',
       });
     return addresses.LocationResult;
