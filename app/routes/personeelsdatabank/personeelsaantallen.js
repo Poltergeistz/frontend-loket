149dfdebcedc13ee7e9707b0b50c9c77e499aefe
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-01-09 03:19:40 +0100

    [personeelsdatabank] shuffling with model and routes

--- app/routes/personeelsdatabank/personeelsaantallen.js
+++ app/routes/personeelsdatabank/personeelsaantallen.js
@@ -1,4 +1,22 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Route.extend({
+export default Route.extend(AuthenticatedRouteMixin, {
+  currentSession: service(),
+
+  beforeModel() {
+    if (!this.currentSession.canAccessPersoneelsbeheer)
+      this.transitionTo('index');
+  },
+
+  async model(){
+    const bestuurseenheid = await this.get('currentSession.group');
+    const datasets = await this.store.query('employee-dataset', {
+      include: 'periods,bestuurseenheid,subjects,periods.time-period',
+      'filter[bestuurseenheid][id]': bestuurseenheid.id
+    });
+    //TODO: what does subjects mean here?
+    return { bestuurseenheid, datasets };
+  }
 });
