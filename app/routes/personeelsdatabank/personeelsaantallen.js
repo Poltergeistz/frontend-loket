8efcf66280f9bbe493db7c61495681d15736ab99
Erika Pauwels <erika.pauwels@gmail.com>
2020-01-13 14:08:25 +0100

    Move authenticated route validation to root route of personeelsbeheer

--- app/routes/personeelsdatabank/personeelsaantallen.js
+++ app/routes/personeelsdatabank/personeelsaantallen.js
@@ -1,14 +1,8 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Route.extend(AuthenticatedRouteMixin, {
+export default Route.extend({
   currentSession: service(),
-
-  beforeModel() {
-    if (!this.currentSession.canAccessPersoneelsbeheer)
-      this.transitionTo('index');
-  },
 
   async model(){
     const bestuurseenheid = await this.get('currentSession.group');
