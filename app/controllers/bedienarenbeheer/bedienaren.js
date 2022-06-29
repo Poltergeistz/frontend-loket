0cf828478789433ececfb0421138459fdacbb73b
Unknown <achrafatauil@hotmail.com>
2022-06-29 09:31:06 +0200

    remove copied over code from mandataris (not needed atm)

--- app/controllers/bedienarenbeheer/bedienaren.js
+++ app/controllers/bedienarenbeheer/bedienaren.js
@@ -10,49 +10,9 @@ export default class BedienarenbeheerBedienarenController extends Controller {
   @service() router;
 
   @tracked bedienarenbeheer;
+
   @tracked filter = '';
   @tracked page = 0;
-  sort = 'is-bestuurlijke-alias-van.achternaam';
+  sort = '';
   size = 20;
-
-  get hasActiveChildRoute() {
-    return (
-      this.router.currentRouteName.startsWith('bedienarenbeheer.bedienaren.') &&
-      this.router.currentRouteName != 'bedienarenbeheer.bedienaren.index'
-    );
-  }
-
-  @alias('bedienarenbeheer.startDate') startDate;
-  @alias('bedienarenbeheer.bestuursperioden') bestuursperioden;
-  @alias('bedienarenbeheer.bestuurseenheid') bestuurseenheid;
-  @alias('bedienarenbeheer.bestuursorganen') bestuursorganen;
-
-  @restartableTask
-  *search(searchData) {
-    yield timeout(300);
-    this.page = 0;
-    this.filter = searchData;
-  }
-
-  @action
-  handleAddMandatarisClick() {
-    if (this.router.currentRouteName === 'bedienarenbeheer.bedienaren.new')
-      this.router.transitionTo('bedienarenbeheer.bedienaren.index');
-    else this.router.transitionTo('bedienarenbeheer.bedienaren.new');
-  }
-
-  @action
-  handleBeheerFractiesClick() {
-    this.router.transitionTo('bedienarenbeheer.fracties');
-  }
-
-  @action
-  selectPeriod(startDate) {
-    this.router.transitionTo('bedienarenbeheer.bedienaren', {
-      queryParams: {
-        page: 0,
-        startDate: startDate,
-      },
-    });
-  }
 }
--- app/routes/bedienarenbeheer/bedienaren.js
+++ app/routes/bedienarenbeheer/bedienaren.js
@@ -1,57 +1,15 @@
 /* eslint-disable ember/no-mixins */
 import Route from '@ember/routing/route';
-import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
 export default class BedienarenbeheerBedienarenRoute extends Route.extend(
   DataTableRouteMixin
 ) {
-  @service store;
+  modelName = 'agentsInPosition';
 
-  modelName = 'bedienaar';
-
-  beforeModel() {
-    const bedienarenbeheer = this.modelFor('bedienarenbeheer');
-    this.bedienarenbeheer = bedienarenbeheer;
-  }
-
-  mergeQueryOptions(params) {
-    const bestuursorganenIds = this.bedienarenbeheer.bestuursorganen.map((o) =>
-      o.get('id')
-    );
-
-    const queryParams = {
-      sort: params.sort,
-      filter: {
-        bekleedt: {
-          'bevat-in': {
-            id: bestuursorganenIds.join(','),
-          },
-        },
-      },
-      include: ['is-bestuurlijke-alias-van', 'bekleedt.bestuursfunctie'].join(
-        ','
-      ),
-    };
-
-    if (params.filter) {
-      queryParams['filter']['is-bestuurlijke-alias-van'] = params.filter;
+  mergeQueryOptions() {
+    return {
+      include: ['is-bestuurlijke-alias-van', 'person', 'post'].join(','),
     }
-
-    return queryParams;
-  }
-
-  setupController(controller) {
-    super.setupController(...arguments);
-    controller.searchData = this.paramsFor('bedienarenbeheer.bedienaren')[
-      'filter'
-    ];
-    controller.bedienarenbeheer = this.bedienarenbeheer;
-  }
-
-  @action
-  reloadModel() {
-    this.refresh();
-  }
+  };
 }
